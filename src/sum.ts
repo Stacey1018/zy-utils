d0abd7e13484156a60611999f0a7a7bfdaa2d392
yangzhang <yangzhang@healthlink.cn>
2025-02-13 14:33:52 +0800

    feat: 🎸 eslint

--- src/sum.ts
+++ src/sum.ts
@@ -1,8 +1,10 @@
+
 /**
- *
- * @param a 数值1
- * @param b 数值2
- * @returns
+ * The function `sum` takes two numbers as input and returns their sum.
+ * @param {number} a - The parameter `a` is a number.
+ * @param {number} b - The parameter `b` in the `sum` function is a number that is being added to the
+ * parameter `a` to calculate the sum.
+ * @returns The sum of the two input numbers, `a` and `b`, is being returned.
  */
 const sum = (a: number, b: number): number => {
   // console.log(a);
